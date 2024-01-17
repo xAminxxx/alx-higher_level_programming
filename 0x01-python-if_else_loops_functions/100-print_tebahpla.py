f51e64c6e407ddadad26f9b8736b6d1c37d87d03
xAminxxx <maknimedamin8@gmail.com>
2024-01-16 18:52:48 -0800

    fst

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-for i in range(122, 64, -2):
-    print('{:c}{:c}'.format(i-32, i), end='')
+for i in range(122, 96, -1):
+    if i % 2:
+        i = i - 32
+    print("{:c}".format(i), end="")
