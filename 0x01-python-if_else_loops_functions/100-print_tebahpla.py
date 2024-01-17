d6c339bd0645c428ee40c8b800a9a1d43bd061b0
xAminxxx <maknimedamin8@gmail.com>
2024-01-16 18:35:53 -0800

    fst

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-for i in range(122, 96, -1):
-    if i % 2:
-        i = i - 32
-    print("{:c}".format(i), end="")
+for i in range(90, 64, -1):
+    print(f'{chr(i + 32)}{chr(i)}', end='')
